d02d08c4aacfdc8721477d7c134b9faf99524916
Marloschi <marlo.mitterlehner@gmx.at>
2022-05-18 10:38:43 +0200

    javascript

---
+++ JS/04.js
@@ -0,0 +1,22 @@
+
+/*for(i = 7; i<=45; i++){
+    console.log(i)
+}
+
+for(i = 9; i<=99; i++){
+    console.log(i)
+}
+
+for(i = 10; i>=0; i--){
+    console.log(i)
+}
+
+for(i = 1234; i>=1207; i--){
+    console.log(i)
+}*/
+
+for(i = 0; i<=7; i++){
+    console.log("Marlo")
+}
+
+for()
