d998b7253f44b35ecfbc107417565c17cf2484f2
Marloschi <marlo.mitterlehner@gmx.at>
2022-05-18 10:49:54 +0200

    yallah

--- JS/04.js
+++ JS/04.js
@@ -1,5 +1,5 @@
 
-/*for(i = 7; i<=45; i++){
+for(i = 7; i<=45; i++){
     console.log(i)
 }
 
@@ -13,10 +13,18 @@ for(i = 10; i>=0; i--){
 
 for(i = 1234; i>=1207; i--){
     console.log(i)
-}*/
+}
 
 for(i = 0; i<=7; i++){
     console.log("Marlo")
 }
 
-for()
+for(i = 0; i <=10; i++){
+    console.log(i)
+    if(i == 10){
+        console.log("Marlo")
+        for(a = 10; a>=0; a--){
+            console.log(a)
+        }
+    }
+}
